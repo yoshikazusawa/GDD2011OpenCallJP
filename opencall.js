3738aa7db942512c2e38d287b9e6ee84eb7da376
Yoshikazu Sawa <yoshikazusawa@github.com>
2011-08-08 22:31:20 +0900

    removed useless codes

--- opencall.js
+++ opencall.js
@@ -16,36 +16,15 @@ function isNotEnteredLogo() {
 }
 
 function onSvgLoad($s) {
-    $s.find('g:nth-child(3)').bind('click', function() {
-        console.log(this);
-    });
-    var targets = $s.find('path[fill=#77a2d9]').filter(isNotEnteredLogo);
-    /*
-    targets
-    .mouseover(function() {
-        $(this).attr({ fill: '#333300'});
-    })
-    .mouseout(function() {
-        $(this).attr({ fill: '#77a2d9'});
-    });
-    */
-
+    var regions = $s.find('path[fill=#77a2d9]').filter(isNotEnteredLogo);
     var colors = ['#871c40', '#2d5434', '#62162e'];
-    targets.each(function(i, e) {
+    regions.each(function(i, e) {
         $(this).attr({ fill: colors[i % 3] });
     });
 
-
-    var locations = $s.find('g:nth-child(1) path[fill=#ffffff]').filter(isNotEnteredLogo);
-    
-    locations.each(function(i) {
-        $(this).attr({ stroke: '#000', 'stroke-width': '3pt' });
-    });
+    var locations = $s.find('path[fill=#ffffff]').filter(isNotEnteredLogo);
 
     locations
-    .click(function() {
-        console.log(this);
-    })
     .mouseover(function() {
         $(this).attr({ fill: '#000' });
     })
